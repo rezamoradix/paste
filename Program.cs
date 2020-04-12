fa80066bc0a24960bb371d7197d4fc329be36ff1
Reza <rezamoradix@gmail.com>
2020-04-12 14:57:55 +0430

    Init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TextCopy;
+
+namespace paste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length > 0)
+                    Clipboard.SetText(args[0]);
+                var clipboard = Clipboard.GetText();
+                Console.WriteLine(clipboard);
+            }
+            catch (System.Exception) { }
+        }
+    }
+}
